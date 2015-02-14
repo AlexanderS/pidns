ac07168dd91fe88ebdd2d74b88b20997f736e707
Alexander Sulfrian <alexander@sulfrian.net>
2015-02-14 01:41:53 +0100

    Add basic sub command matching and a short help.

--- pidns.c
+++ pidns.c
@@ -2,8 +2,70 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char** argv) {
-
-
+int list(void)
+{
     return EXIT_SUCCESS;
+}
+
+int add(int argc, char** argv)
+{
+    return EXIT_SUCCESS;
+}
+
+int exec(int argc, char** argv)
+{
+    return EXIT_SUCCESS;
+}
+
+int delete(int argc, char** argv)
+{
+    return EXIT_SUCCESS;
+}
+
+void usage(void)
+{
+    puts("Usage: pidns list\n"
+         "       pidns add NAME cmd ...\n"
+         "       pidns exec NAME cmd ...\n"
+         "       pidns delete NAME");
+}
+
+int main(int argc, char** argv)
+{
+    int i;
+
+    for (i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "--help") == 0) {
+            usage();
+            return EXIT_SUCCESS;
+        }
+    }
+
+    if (argc <= 1) {
+        return list();
+    }
+
+    if (strcmp(argv[1], "help") == 0) {
+        usage();
+        return EXIT_SUCCESS;
+    }
+
+    if (strcmp(argv[1], "list") == 0) {
+        return list();
+    }
+
+    if (strcmp(argv[1], "add") == 0) {
+        return add(argc - 1, argv+1);
+    }
+
+    if (strcmp(argv[1], "exec") == 0) {
+        return exec(argc - 1, argv+1);
+    }
+
+    if (strcmp(argv[1], "delete") == 0) {
+        return delete(argc - 1, argv+1);
+    }
+
+    usage();
+    return EXIT_FAILURE;
 }
