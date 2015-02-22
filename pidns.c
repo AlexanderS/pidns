87cbaf45dfe676faf5e4ca70219f5c30acdea50e
Alexander Sulfrian <alexander@sulfrian.net>
2015-02-22 23:05:45 +0100

    Implement add and exec.
    The implementation of add and exec are similar to the ones of ip
    netns. Here we do not mount the namespace file directly, but the
    namespace directory of the process it self. This has the advantage
    that the namespace files disappears if the process dies.

--- pidns.c
+++ pidns.c
@@ -1,11 +1,14 @@
 #include <dirent.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <sched.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/mount.h>
 #include <sys/param.h>
+#include <sys/stat.h>
 
 #define PIDNS_RUN_DIR "/var/run/pidns"
 
@@ -62,6 +65,8 @@ int list(void)
 int add(int argc, char** argv)
 {
     const char *name, *cmd;
+    char pidns_path[MAXPATHLEN];
+    int made_pidns_run_dir_mount = 0;
 
     if (argc < 1) {
         fprintf(stderr, "No pidns name specified\n");
@@ -78,12 +83,76 @@ int add(int argc, char** argv)
         namespace_cleanup(name);
     }
 
-    return EXIT_SUCCESS;
+    /* Create the base pidns directory if it doesn't exist */
+    mkdir(PIDNS_RUN_DIR, S_IRWXU|S_IRGRP|S_IXGRP|S_IROTH|S_IXOTH);
+
+    /* Make it possible for network namespace mounts to propogate between
+     * mount namespaces.  This makes it likely that a unmounting a network
+     * namespace file in one namespace will unmount the network namespace
+     * file in all namespaces allowing the network namespace to be freed
+     * sooner.
+     */
+    while (mount("", PIDNS_RUN_DIR, "none", MS_SHARED | MS_REC, NULL)) {
+        /* Fail unless we need to make the mount point */
+        if (errno != EINVAL || made_pidns_run_dir_mount) {
+            fprintf(stderr, "mount --make-shared %s failed: %s\n",
+                    PIDNS_RUN_DIR, strerror(errno));
+            return EXIT_FAILURE;
+        }
+
+        /* Upgrade PIDNS_RUN_DIR to a mount point */
+        if (mount(PIDNS_RUN_DIR, PIDNS_RUN_DIR, "none", MS_BIND, NULL)) {
+            fprintf(stderr, "mount --bind %s %s failed: %s\n",
+                    PIDNS_RUN_DIR, PIDNS_RUN_DIR, strerror(errno));
+            return EXIT_FAILURE;
+        }
+        made_pidns_run_dir_mount = 1;
+    }
+
+    /* Create the filesystem state */
+    snprintf(pidns_path, sizeof(pidns_path), "%s/%s/", PIDNS_RUN_DIR, name);
+    if (mkdir(pidns_path, S_IRWXU|S_IRGRP|S_IXGRP|S_IROTH|S_IXOTH) < 0) {
+        fprintf(stderr, "Cannot not create namespace dir \"%s\": %s\n",
+                pidns_path, strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    /* Create the new pid namespace */
+    if (unshare(CLONE_NEWPID) < 0) {
+        fprintf(stderr, "unshare failed: %s\n", strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    /* Bind the namespace directory to get the pid namespace later */
+    if (mount("/proc/self/ns/", pidns_path, "none", MS_BIND, NULL) < 0) {
+        fprintf(stderr, "Bind /proc/self/ns/ -> %s failed: %s\n",
+                pidns_path, strerror(errno));
+    }
+
+    if (unshare(CLONE_NEWNS) < 0) {
+        fprintf(stderr, "unshare failed: %s\n", strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    /* Don't let any mounts propogate back to the parent */
+    if (mount("", "/", "none", MS_SLAVE | MS_REC, NULL)) {
+        fprintf(stderr, "\"mount --make-rslave /\" failed: %s\n",
+                strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    if (execvp(cmd, argv + 1) < 0)
+        fprintf(stderr, "exec of \"%s\" failed: %s\n",
+                cmd, strerror(errno));
+
+    return EXIT_FAILURE;
 }
 
 int exec(int argc, char** argv)
 {
     const char *name, *cmd;
+    char pidns_path[MAXPATHLEN];
+    int pidns;
 
     if (argc < 1) {
         fprintf(stderr, "No pidns name specified\n");
@@ -100,7 +169,39 @@ int exec(int argc, char** argv)
         namespace_cleanup(name);
     }
 
-    return EXIT_SUCCESS;
+    /* Get the pid namespace from the bind mounted directory */
+    snprintf(pidns_path, sizeof(pidns_path), "%s/%s/pid", PIDNS_RUN_DIR, name);
+    pidns = open(pidns_path, O_RDONLY);
+    if (pidns < 0) {
+        fprintf(stderr, "Cannot open pid namespace \"%s\": %s\n",
+                name, strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    /* Set the target pid namespace */
+    if (setns(pidns, CLONE_NEWPID) < 0) {
+        fprintf(stderr, "seting the pid namespace \"%s\" failed: %s\n",
+                name, strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    if (unshare(CLONE_NEWNS) < 0) {
+        fprintf(stderr, "unshare failed: %s\n", strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    /* Don't let any mounts propogate back to the parent */
+    if (mount("", "/", "none", MS_SLAVE | MS_REC, NULL)) {
+        fprintf(stderr, "\"mount --make-rslave /\" failed: %s\n",
+                strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    if (execvp(cmd, argv + 1) < 0)
+        fprintf(stderr, "exec of \"%s\" failed: %s\n",
+                cmd, strerror(errno));
+
+    return EXIT_FAILURE;
 }
 
 int delete(int argc, char** argv)
